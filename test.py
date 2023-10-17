13ebf2a83c2ef77b7e03557f4e454229b6e1ce2e
Ozan Berk Gultegin <anurathvanryu@gmail.com>
2023-10-17 11:18:19 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def sigmoid(t):
+    return 1 / (1 + np.exp(-t))
+def logloss(p, y):
+    J_theta = - (y * np.log(p) + (1 - y) * np.log(1 - p))
+    return J_theta
+
+def gradientDescent(x_input, y_label, alpha):
+    #theta = theta - alpha * np.gradient(logloss(p,y))
+    theta = np.random.rand()
+    #theta = theta - alpha * np.gradient()
+    p = sigmoid(x_input[1][0])
+    logloss(p,y_label[1][0])
+    print(logloss(p,y_label[1][0]))
+
+learning_rate = 2
+
+x_input = np.array([[29],
+                    [15],
+                    [33],
+                    [28],
+                    [39]])
+y_label = np.array([[1],
+                    [0],
+                    [1],
+                    [0],
+                    [1]])
+#gradientDescent(x_input,y_label,learning_rate)
+gradientDescent(x_input,y_label,learning_rate)
